26e5b7cb2c34ff8554c14482186316c0b2a50826
Pedro Henrique Alves <pepe.henrike007@outlook.com>
2022-10-18 11:45:22 -0300

    Atualização dos arquivos
    - Mudança no png
    - Finalização do código

--- Main.py
+++ Main.py
@@ -16,9 +16,11 @@ df.drop(["REGISTRO_DISTRIBUIDORA", "CNPJ_DISTRIBUIDORA","CPB_ROE","ID_PACOTE","N
 df.dropna(subset=["TITULO_ORIGINAL"], inplace=True)
 
 
+
 #Edição dos nomes das colunas
 df = df.rename(columns={"RAZAO_SOCIAL_DISTRIBUIDORA":"DISTRIBUIDORA","TITULO_ORIGINAL":"TITULO ORIGINAL", "TITULO_BRASILEIRO":"TITULO BRASILEIRO",
                         "PAIS_OBRA":"PAIS DE ORIGEM","MODALIDADE_VENDAS":"MODALIDADE DE VENDAS","TIPO_SUPORTE":"TIPO DE SUPORTE", "NR_COPIAS":"TOTAL DE COPIAS"})
+
 
 
 #Qual a proporção entre vendidos e devolvidos?
@@ -30,21 +32,32 @@ plt.style.use("classic")
 fig1, grafico = plt.subplots()
 grafico.pie(sizes, labels=labels, autopct='%1.1f%%', colors=("limegreen","cornflowerblue"))
 
+grafico.set_title('Vendidos vs Devolvidos')
+
 plt.savefig("VendidosXDevolvidos.png")
-#plt.show()
+plt.show()
 
 #Qual/Quais a/s obra/s mais vendida?
 
-print(df.nlargest(10,"TOTAL DE COPIAS")[["TOTAL DE COPIAS","TITULO ORIGINAL"]])
+print(df.nlargest(10,"TOTAL DE COPIAS")[["TITULO ORIGINAL","TOTAL DE COPIAS"]])
 
 #Vendas por mês.
-#Devoluções por mês.
-#Quais são os paises de origem tem mais copias vendidas?
+
+total_vendas_mes = df.loc[df["MOVIMENTO"] == "VENDA"].groupby("MES")["TOTAL DE COPIAS"].sum().unique()
+mes = df["MES"].sort_values().unique()
+plt.style.use("classic")
+fig1, grafico = plt.subplots()
+grafico.bar(mes,total_vendas_mes, label = mes)
+
+grafico.set_ylabel('Total de vendas')
+grafico.set_xlabel('Mês')
+grafico.set_title('Vendas do ano de 2021')
+
+plt.savefig("Vendas_do_ano_de_2021.png")
+plt.show()
 
 
-#print(df["MOVIMENTO"].describe())
-print(df.dtypes)
-
+#Escreve o novo conjunto de dados
 df.to_excel("Tabela_de_Amostras.xlsx", index = False)
 
 
